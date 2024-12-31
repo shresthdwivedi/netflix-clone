54bda12cb21b23f3aa9486f679bc5d3658790c13
Shresth Dwivedi <shresth0109@gmail.com>
2024-12-31 17:19:39 +0530

    auth UI completed

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,6 +1,21 @@
-import Input from '@/components/input'
+'use client'
+
+import Input from "@/components/input";
+import { useCallback, useState } from "react";
 
 const Auth = () => {
+    
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [variant, setVariant] = useState('login');
+
+    const toggleVariant = useCallback(() => {
+        setVariant(variant === 'login' ? 'register' : 'login');
+    }
+    ,[variant])
+
     return (
         <>
             <div className="relative h-full w-full bg-[url('/images/hero.png')] bg-no-repeat bg-center bg-cover bg-fixed">
@@ -11,13 +26,42 @@ const Auth = () => {
                     <div className="flex justify-center ">
                         <div className="bg-black bg-opacity-70 p-16 self-center mt-10 lg:w-2/4 lg:max-w-md rounded-md w-full">
                             <h2 className="text-4xl text-white font-semibold mb-8">
-                                Sign In
+                                {variant === 'login' ? 'Sign In' : 'Register'}    
                             </h2>
-                            <div className='flex flex-col gap-4'>
-                                <Input />
-                                <Input />
-                            
+                            <div className='flex flex-col gap-4 '>
+                                {variant === 'register' && (
+                                    <Input 
+                                        id="name"
+                                        label="Username"
+                                        onChange={(e:any) => {setName(e.target.value)}}
+                                        type="name"
+                                        value={name}
+                                    />
+                                )}
+                                <Input 
+                                    id="email"
+                                    label="Email"
+                                    onChange={(e:any) => {setEmail(e.target.value)}}
+                                    type="email"
+                                    value={email}
+                                />   
+                                <Input 
+                                    id="password"
+                                    label="Password"
+                                    onChange={(e:any) => {setPassword(e.target.value)}}
+                                    type="password"
+                                    value={password}
+                                />                          
                             </div>
+                            <button className="bg-red-600 w-full text-white py-3 mt-10 font-bold rounded-md hover:bg-red-700 transition">
+                                {variant === 'login' ? 'Login' : 'Sign Up'}
+                            </button>
+                            <p className="text-neutral-500 mt-12">
+                                    {variant === 'login' ? "Don't have an account?" : 'Already have an account?'}
+                                <span onClick={toggleVariant} className="ml-1 text-white font-bold cursor-pointer hover:underline ">
+                                    {variant === 'login' ? 'Create an Account' : 'Sign In'}
+                                </span>
+                            </p>
                         </div>
                     </div>
                 </div>
--- components/input.tsx
+++ components/input.tsx
@@ -1,6 +1,32 @@
-const Input = () => {
+interface InputProps {
+    id: string,
+    onChange: any,
+    value: string,
+    label: string,
+    type?: string,
+}
+
+const Input: React.FC<InputProps> = ({
+    id,
+    onChange,
+    value,
+    label,
+}) => {
     return (
-        <input/>
+        <div className="relative">
+            <input
+                id={id}
+                onChange={onChange}
+                value={value}
+                className="block rounded-md w-full px-6 pt-6 pb-1 text-md text-white bg-neutral-700 appearance-none focus:outline-none focus:ring-2 focus:ring-offset-4 focus:ring-slate-50 focus:ring-offset-slate-900 peer"
+                placeholder=" "
+            />
+            <label 
+                htmlFor={label}
+                className="absolute text-md text-zinc-400 duration-150 transform-translate-y-3scale-75 top-4 left-6 z-10 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-3">
+                {label}
+            </label>
+        </div>
     );
 }
 
